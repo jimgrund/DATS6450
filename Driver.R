1734821b9903cc5b72d7925ff5e08498179baba5
jassalak <akash.jassal09@gmail.com>
2018-06-22 14:37:49 -0400

    Update Driver.R
    This form is knitd

--- Driver.R
+++ Driver.R
@@ -1,11 +1,37 @@
-# Example for Jags-Ybinom-XnomSsubjCcat-MbinomBetaOmegaKappa.R 
-#------------------------------------------------------------------------------- 
-# Optional generic preliminaries:
-graphics.off() # This closes all of R's graphics windows.
-rm(list=ls())  # Careful! This clears all of R's memory!
-#------------------------------------------------------------------------------- 
+#' ---
+#' title: 6450 BayesianMethods Project (Driver File)
+#' author: TeamPAJ
+#' date: 25June2018
+#' output:
+#'    html_document:
+#'      toc: true
+#'      highlight: haddock
+#' ---
 
-library(dplyr)
+# 
+#
+#
+
+#'#######################################
+
+#' ## Environment Preparation
+
+# Remove any objects in the Environment
+rm(list = ls())
+
+# Closes all of R's graphics windows
+graphics.off()
+
+# Knitr global options
+library(knitr)
+opts_chunk$set(eval = TRUE, echo = TRUE, warning = FALSE,
+               tidy = TRUE, results = "hold", cache = TRUE)
+
+# Load Necessary Libraries
+suppressPackageStartupMessages(library(dplyr))
+
+# Set the overall seed for reproducibility
+set.seed(6450)
 
 # initialize the working directory for the final project
 jim_dir = "/Users/jimgrund/Documents/GWU/Bayesian_Methods/FinalProject/"
@@ -13,11 +39,26 @@ akash_dir = "C:/Users/akash/Desktop/GWU/6450_Bayesian_YHuang/project/project"
 patrick_dir = "/Users/pjordan/Documents/GWU/6450/FinalProject"
 
 for (directory in c(akash_dir, jim_dir, patrick_dir)) {
-   if ( dir.exists(directory) ) {
-      setwd(directory)
-      break
-   }
+  if ( dir.exists(directory) ) {
+    setwd(directory)
+    break
+  }
 }
+
+# Load the relevant model into R's working memory:
+source("Model.R")
+
+#' ### Graph Options
+
+# Optional: Specify filename root and graphical format for saving output.
+# Otherwise specify as NULL or leave saveName and saveType arguments 
+# out of function calls.
+fileNameRoot = "graph_" 
+graphFileType = "png" 
+
+#'#######################################
+
+#' ## Data Load & Tidy
 
 # Load the Breast Cancer dataset
 myData = read.csv("data.csv")
@@ -28,19 +69,15 @@ myData$tests <- 1
 myData <- mutate(myData, diagnosis_code = ifelse(diagnosis == 'B',0,1))
 
 # plot a histogram just to see the distribution of those bins
-hist(as.numeric(myData$radius_bin))
+hist(as.numeric(myData$radius_bin), col = 'blue4')
 
 
-#------------------------------------------------------------------------------- 
-# Load the relevant model into R's working memory:
-source("Model.R")
-#------------------------------------------------------------------------------- 
-# Optional: Specify filename root and graphical format for saving output.
-# Otherwise specify as NULL or leave saveName and saveType arguments 
-# out of function calls.
-fileNameRoot = "BreastCancer-POST-" 
-graphFileType = "png" 
-#------------------------------------------------------------------------------- 
+#'#######################################
+
+#' ## MCMC Chain
+
+#' ### Generate
+
 # Generate the MCMC chain:
 startTime = proc.time()
 mcmcCoda = genMCMC( data=myData , 
@@ -50,25 +87,33 @@ mcmcCoda = genMCMC( data=myData ,
 stopTime = proc.time()
 elapsedTime = stopTime - startTime
 show(elapsedTime)
-#------------------------------------------------------------------------------- 
+
+
+#' ### Diagnostics 
+
 # Display diagnostics of chain, for specified parameters:
 parameterNames = varnames(mcmcCoda) # get all parameter names for reference
 for ( parName in c("omega[1]","omegaO","kappa[1]","kappaO","theta[1]") ) { 
   diagMCMC( codaObject=mcmcCoda , parName=parName , 
-                saveName=fileNameRoot , saveType=graphFileType )
+            saveName=fileNameRoot , saveType=graphFileType )
 }
-#------------------------------------------------------------------------------- 
+
+#' ### Summary Statistics
+
 # Get summary statistics of chain:
-summaryInfo = smryMCMC( mcmcCoda , compVal=NULL , 
-                        diffSVec=c(75,156, 159,844) , 
-                        diffCVec=c(1,2,3) , 
+summaryInfo = smryMCMC( mcmcCoda , compVal=NULL ,
+                        diffSVec=c(75,156, 159,844) ,
+                        diffCVec=c(1,2,3) ,
                         compValDiff=0.0 , saveName=fileNameRoot )
+
+#' ### Graph
+
 # Display posterior information:
-plotMCMC( mcmcCoda , data=myData , 
+plotMCMC( mcmcCoda , data=myData ,
           zName="diagnosis_code", NName="tests", sName="id", cName="radius_bin",
           compVal=NULL ,
-          diffCList=list( c(1,2), c(2,3) ) , 
-          diffSList=list( c("842302","842517"), c("84501001","84458202") ) , 
+          diffCList=list( c(1,2), c(2,3) ) ,
+          diffSList=list( c("842302","842517"), c("84501001","84458202") ) ,
           compValDiff=0.0, #ropeDiff = c(-0.05,0.05) ,
           saveName=fileNameRoot , saveType=graphFileType )
-#------------------------------------------------------------------------------- 
+#'#######################################
